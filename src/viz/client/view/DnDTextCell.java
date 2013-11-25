f4b064d02167c90ec8d357f822582197811ed2dc
Thomas Rodenhausen <thomas.rodenhausen@gmail.com>
2013-11-24 22:37:58 -0700

    create consumedevents only once, make unmodifiable set

--- src/viz/client/view/DnDTextCell.java
+++ src/viz/client/view/DnDTextCell.java
@@ -1,31 +1,45 @@
 package viz.client.view;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.google.gwt.cell.client.AbstractSafeHtmlCell;
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.cell.client.Cell.Context;
 import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.text.shared.SafeHtmlRenderer;
+import com.google.gwt.text.shared.SimpleSafeHtmlRenderer;
 
 public class DnDTextCell extends TextCell {
 
+	private Set<String> consumedEvents;
+	
+	public DnDTextCell() {
+		super();
+		consumedEvents = new HashSet<String>();
+		consumedEvents.add(BrowserEvents.CLICK);
+		consumedEvents.add(BrowserEvents.DBLCLICK);
+		consumedEvents.add(BrowserEvents.CONTEXTMENU);
+		consumedEvents.add(BrowserEvents.DRAG);
+		consumedEvents.add(BrowserEvents.DRAGEND);
+		consumedEvents.add(BrowserEvents.DRAGENTER);
+		consumedEvents.add(BrowserEvents.DRAGLEAVE);
+		consumedEvents.add(BrowserEvents.DRAGOVER);
+		consumedEvents.add(BrowserEvents.DRAGSTART);
+		consumedEvents.add(BrowserEvents.DROP);
+		consumedEvents = Collections.unmodifiableSet(consumedEvents);
+	}
+
 	@Override
 	public Set<String> getConsumedEvents() {
-		Set<String> result = new HashSet<String>();
-		result.add(BrowserEvents.CLICK);
-		result.add(BrowserEvents.DBLCLICK);
-		result.add(BrowserEvents.CONTEXTMENU);
-		result.add(BrowserEvents.DRAG);
-		result.add(BrowserEvents.DRAGEND);
-		result.add(BrowserEvents.DRAGENTER);
-		result.add(BrowserEvents.DRAGLEAVE);
-		result.add(BrowserEvents.DRAGOVER);
-		result.add(BrowserEvents.DRAGSTART);
-		result.add(BrowserEvents.DROP);
-		return result;
+		return this.consumedEvents;
 	}
 	
 	@Override
